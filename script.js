69422e12dfda0fa5e4a02ad13f4d7ce246679ae9
cookie <cookie@debian-BULLSEYE-live-builder-AMD64>
2023-04-05 14:37:05 +0300

    start JS cours

---
+++ script.js
@@ -0,0 +1,9 @@
+const numberOfFilms = prompt('Сколько фильмов было посмотрено'); 
+
+const pesonalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
